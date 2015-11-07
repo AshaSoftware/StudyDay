34bf7f6e1271f0f82c20ffe87a3335b415b69335
Tiago Henrique <tiago.henrique.cco@gmail.com>
2015-11-07 11:23:15 -0200

    Trocado o menu padrão por um menu de navegação

--- app/src/main/java/com/ashasoftware/studyday/MainActivity.java
+++ app/src/main/java/com/ashasoftware/studyday/MainActivity.java
@@ -2,13 +2,18 @@ package com.ashasoftware.studyday;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
@@ -21,6 +26,11 @@ public class MainActivity extends AppCompatActivity {
             actionBar.setHomeAsUpIndicator( R.drawable.ic_menu_white );
             actionBar.setDisplayHomeAsUpEnabled( true );
         }
+
+        drawer = (DrawerLayout) findViewById( R.id.drawer_layout );
+
+        NavigationView navigationView = (NavigationView) findViewById( R.id.drawer_menu_main );
+        navigationView.setNavigationItemSelectedListener( this );
     }
 
     @Override
@@ -33,16 +43,36 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected( MenuItem item ) {
         switch( item.getItemId() ) {
-            case R.id.show_subject:
-                Intent i = new Intent( this, SubjectViewActivity.class );
-                startActivity( i );
+            case android.R.id.home:
+                if( !drawer.isDrawerOpen( GravityCompat.START ) )
+                    drawer.openDrawer( GravityCompat.START );
+                else
+                    drawer.closeDrawer( GravityCompat.START );
                 return true;
-            case R.id.show_naoescolar:
-                Intent j = new Intent( this, NaoEscolarViewActivity.class );
-                startActivity( j );
-                return true;
+
         }
 
         return super.onOptionsItemSelected( item );
+    }
+
+    @Override
+    public boolean onNavigationItemSelected( MenuItem menuItem ) {
+
+        switch( menuItem.getItemId() ) {
+            case R.id.drawer_item_subject:
+                Intent i = new Intent( this, SubjectViewActivity.class );
+                startActivity( i );
+                break;
+            case R.id.drawer_item_non_school:
+                Intent j = new Intent( this, NaoEscolarViewActivity.class );
+                startActivity( j );
+                break;
+            default:
+                return false;
+        }
+
+        drawer.closeDrawers();
+
+        return true;
     }
 }
